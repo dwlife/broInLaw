a50f48c6b5b84573d55f61302bb46eef6a52e801
hiYuzu <pomelo.yuzu1997@gmail.com>
2019-04-24 15:24:04 +0800

    使筛选更严格

--- src/main/webapp/static/js/updateQuestion.js
+++ src/main/webapp/static/js/updateQuestion.js
@@ -9,13 +9,13 @@ function updateQuestion() {
     var questionText = document.getElementById("questionTextArea").value;
     var answerText = document.getElementById("answerTextArea").value;
     var tailNote = document.getElementById("tailNoteArea").value;
-    if (questionText == "输入问题...") {
+    if (questionText === "输入问题..." || questionText.trim().length === 0) {
         alert("请输入问题！");
         return false;
-    } else if (answerText == "输入答案...") {
+    } else if (answerText === "输入答案..." || answerText.trim().length === 0) {
         alert("请输入答案！");
         return false;
-    } else if (tailNote == "输入问题补充...") {
+    } else if (tailNote === "输入问题补充..." || tailNote.trim().length === 0) {
         tailNote = "";
     }
     document.getElementById("messageForm").reset();
